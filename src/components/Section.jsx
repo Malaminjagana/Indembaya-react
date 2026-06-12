1ee1d7b958cdb87996bc957939bf61b9040d1d4a
malamin jagana <jaganalamin@gmail.com>
2026-06-12 15:00:38 -0700

    new deatails

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -7,7 +7,7 @@ function Section() {
     <section className="section">
       <p>{t("You can  change the language by simply clicking the button above. /Vous pouvez changer la langue en cliquant simplement sur le bouton ci-dessus /يمكنك تغيير اللغة بمجرد النقر على الزر في الأعلى")}</p>
       <ul>
-        <h1>Group A</h1>
+        <h1>Group A 2025</h1>
            <li>Ali Mamane Jagana (2024/2025) 500$ ✅</li>
         <li>Sulayman Janjume Jagana (2024/2025) 500€ ✅</li>
         <li>Bacary Hatou Jagana (2024/2025) 500€ ✅</li>
@@ -45,21 +45,28 @@ function Section() {
           
          
 
-        <h1>Group B</h1>
+        <h1>Group B 2025</h1>
         <li>Banuma Jagana (2024/2025) 500$ ✅</li>
-        <li> Bacary Numa Jagana (2024/2025) 500€ ✅❌ Half is not paid</li>
+        <li> Bacary Numa Jagana (2024/2025) 500€ ✅</li>
         <li>Malamin Tabia Jagana (2024/2025) 500€ ✅</li>
         <li>Ansuma Hatu Jagana (2024/2025) 500€ ❌</li>
-        <li>Osman Tabia Jagana (2024/2025) 500€ ❌</li>
+        <li>Osman Tabia Jagana (2024/2025) 500€ ✅</li>
         <li>Malamin Kulle Jagana (2024/2025) 500$ ❌</li>
         <li>Lassana Kulle Jagana (2024/2025) 500$ ❌</li>
         <li>Ebrahima Gana (2024/2025) 500$ ❌</li>
         <li>Mahmud Essa (2024/2025) 500€ ✅</li>
        
        <h1>Group A 2026 </h1>
+       
        <h1>Group B 2026</h1>
 
+                 <h1>Spending 2025</h1>
+
+                 <li> 30.000 GMD for meate</li>
+
+
       </ul>
+      <h1>2026 Balance: 25,750 GMD</h1>
     </section>
   );
 }
